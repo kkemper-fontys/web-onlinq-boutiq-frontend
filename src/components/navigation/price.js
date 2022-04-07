c6fa65e5806bb46bfc899819a68753601abffe32
Kees Kemper <kees@onlinq.intern>
2022-04-07 13:25:14 +0200

    added range slider to pricerange search

--- src/components/navigation/price.js
+++ src/components/navigation/price.js
@@ -1,3 +1,6 @@
+import React, {useEffect, useState} from "react";
+import {Range, getTrackBackground} from "react-range";
+
 (function ($) {
     $.fn.currencyInput = function () {
         this.each(function () {
@@ -23,15 +26,85 @@ $(document).ready(function () {
 });
 
 const Price = () => {
+    const STEP = 1;
+    const MIN = 0;
+    const MAX = 100;
+    const [values, setValues] = useState([0, 100]);
+
     return (
         <>
             <h4>Prijs</h4>
             <div className={"price"}>
                 <div className={"price-range"}>
-                    <input className={"currency"} type={"number"} name={"price_from"} step={1} min={0} value={0}/>
+                    <input className={"currency"} type={"number"} name={"price_from"} step={1} min={0} value={values[0]} onChange={(value) => {console.log(value)}}/>
                     <p>tot</p>
-                    <input className={"currency"} type={"number"} name={"price_to"} step={1} min={0} value={0}/>
+                    <input className={"currency"} type={"number"} name={"price_to"} step={1} min={0} value={values[1]} onChange={(value) => {console.log(value)}}/>
                 </div>
+                <Range
+                    values={values}
+                    step={STEP}
+                    min={MIN}
+                    max={MAX}
+                    onChange={values => {
+                        setValues(values);
+                    }}
+                    renderTrack={({props, children}) => (
+                        <div
+                            onMouseDown={props.onMouseDown}
+                            onTouchStart={props.onTouchStart}
+                            style={{
+                                ...props.style,
+                                height: "50px",
+                                display: "flex",
+                                width: "100%"
+                            }}
+                        >
+                            <div
+                                ref={props.ref}
+                                style={{
+                                    height: "5px",
+                                    width: "calc(100% - 26px)",
+                                    marginLeft: "13px",
+                                    marginTop: "15px",
+                                    borderRadius: "4px",
+                                    background: getTrackBackground({
+                                        values,
+                                        colors: ["#ccc", "#823557", "#ccc"],
+                                        min: MIN,
+                                        max: MAX
+                                    }),
+                                    alignSelf: "center"
+                                }}
+                            >
+                                {children}
+                            </div>
+                        </div>
+                    )}
+                    renderThumb={({props, isDragged}) => (
+                        <div
+                            {...props}
+                            style={{
+                                ...props.style,
+                                height: "24px",
+                                width: "24px",
+                                borderRadius: "50px",
+                                backgroundColor: "#FFF",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                boxShadow: "0px 2px 6px #AAA",
+                            }}
+                        >
+                            <div
+                                style={{
+                                    height: "16px",
+                                    width: "5px",
+                                    backgroundColor: isDragged ? "#fff" : "#fff"
+                                }}
+                            />
+                        </div>
+                    )}
+                />
             </div>
         </>
     );
