8672349dfe608529cc8bcf63c420afa48b100317
Kees Kemper <kees@onlinq.intern>
2022-04-07 16:46:35 +0200

    added searchbar to navigation panel

--- src/components/content/navigation/navigation.js
+++ src/components/content/navigation/navigation.js
@@ -1,10 +1,12 @@
 import Tags from "./tags";
 import Price from "./price";
 import Sale from "./sale";
+import Searchbar from "./search";
 
 const Navigation = () => {
     return (
         <div className={"navigation"}>
+            <Searchbar />
             <Tags/>
             <Price/>
             <Sale/>
---
+++ src/components/content/navigation/search.js
@@ -0,0 +1,11 @@
+const Searchbar = () => {
+    return (
+        <div className={"searchbar"}>
+            <div className={"searchbar-wrapper"}>
+                <input type={"text"} placeholder={"Zoeken naar"} className={"searchbar-inputfield"} onChange={''}/>
+            </div>
+        </div>
+    )
+}
+
+export default Searchbar;
